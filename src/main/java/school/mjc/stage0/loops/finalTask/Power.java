6fe590e8aa7e8b281ed3e5f3c7a9a890572db4b6
otoycowboy <dennissmart777@gmail.com>
2023-08-31 19:05:15 +0200

    Task completed and tested

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,11 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int result = numberToPrint;
+        for (int i = 0; i < power - 1; i++) {
+            result *= numberToPrint;
+        }
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
